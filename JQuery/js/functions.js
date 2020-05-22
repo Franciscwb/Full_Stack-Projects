e568e508792121018009d2810342eb1c468de089
Francis de Almeida <studioarcadacwb@gmail.com>
2020-05-22 02:53:06 -0300

    first commit from PC

---
+++ JQuery/js/functions.js
@@ -0,0 +1,22 @@
+//Primeira forma de chamr o jquery
+
+$(function(){
+	alert("Hello World!");
+});
+
+
+
+//Executa depois do js para melhor carregar página
+$(window).on('load',function(){
+})
+
+
+
+//Segunda forma de chamar o jquery
+/*
+$(document).ready(function(){
+	alert("Hello World!");
+})
+*/
+
+
---
+++ JavaScript/Aulas Práticas/functions.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+	var repeticao = 0;
+
+	while(nome == 'Guilherme'){
+		console.log("Olá mundo");
+		nome = 'joao';
+	}
+
+	console.log("cheguei até o final!");
+}
